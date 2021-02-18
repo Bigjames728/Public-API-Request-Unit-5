3c23d7f8f2c008af26b8413226cb93784bc75289
Bigjames728 <bigjames728@gmail.com>
2021-02-18 14:26:49 -0800

    Updates to generatePerson function

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,18 @@ fetch('https://randomuser.me/api/?results=12&inc=name,location,email,picture')
 
 function generatePerson(data) {
     const gallery = document.querySelector('.gallery');
-    const html = `<img src='${data}' alt>`;
+    const html = `
+    <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${data.results[0].picture.large}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${data.results[0].name.first} ${data.results[0].name.last}</h3>
+            <p class="card-text">${data.results[0].email}</p>
+            <p class="card-text cap">${data.results[0].city}, ${data.results[0].state}</p>
+        </div>
+    </div>
+    `;
 
 
 
