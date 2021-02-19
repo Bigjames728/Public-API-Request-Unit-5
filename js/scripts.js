948482432bace85ab7f7073b576cf33a59929563
Bigjames728 <bigjames728@gmail.com>
2021-02-19 13:32:27 -0800

    Still trying to get the 12 users to show up in DOM

--- js/scripts.js
+++ js/scripts.js
@@ -13,25 +13,30 @@ function fetchData(url) {
 
 function generatePerson(data) {
     const gallery = document.querySelector('.gallery');
-    const html = `
-    <div class="card">
-        <div class="card-img-container">
-            <img class="card-img" src="${data.results[0].picture.large}" alt="profile picture">
+    const person = data.results;
+    const html = '';
+    data.results.forEach((emp) =>
+        html += `
+        <div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="${person[0].picture.large}" alt="profile picture">
+                </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${person[0].name.first} ${person[0].name.last}</h3>
+                <p class="card-text">${person[0].email}</p>
+                <p class="card-text cap">${person[0].location.city}, ${person[0].location.state}</p>
+            </div>
         </div>
-        <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${data.results[0].name.first} ${data.results[0].name.last}</h3>
-            <p class="card-text">${data.results[0].email}</p>
-            <p class="card-text cap">${data.results[0].location.city}, ${data.results[0].location.state}</p>
-        </div>
-    </div>
-    `;
+        `);
     gallery.insertAdjacentHTML('beforeend', html);
-    console.log(data.results);
+    // person.forEach(generatePerson);
+    
 };
+
 
 
 
 fetchData('https://randomuser.me/api/?results=12&inc=name,location,email,picture')
 
-// parseResponseToJson.forEach(generatePerson(data.results))
+
 
