c38f2fa3b9d01d5d76f71d3c92c42f57709d1595
Bigjames728 <bigjames728@gmail.com>
2021-02-22 14:07:59 -0800

    updates to generateModal() and updateModal()

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,15 @@
 
 const parseResponseToJson = (res) => res.json();
 const generatePersonOnPage = (data) => generatePerson(data);
-
+let arrayOfPeople;
 
 
 function fetchData(url) {
     return fetch(url)
             .then(parseResponseToJson)
             .then(generatePersonOnPage)
+            .then(generateModal)
+            
 }
 
 function generatePerson(data) {
@@ -41,61 +43,65 @@ fetchData('https://randomuser.me/api/?results=12&inc=name,gender,location,email,
 
 
 
-function generateModal(id) {
-    let modalContainer = document.querySelector(".modal-container");
-    let el = document.getElementById(id);
-    let body = document.querySelector("body");
-    let bg = document.createElement("div");
-    bg.className = "modal-container";
+function generateModal() {
+    let html = `
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            
+        </div>
+    </div>
+    `;
 
-    let html = '';
+    gallery.insertAdjacentHTML('afterend', html)
+    document.querySelector(".modal-container").style.display = "block"; // I did this for now so that the modal shows up on refresh of page. Trying to get correct info to show up in modal.
 
-    data.results.forEach((emp) => 
-        html += `
-        <div class="modal-container">
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-                <img class="modal-img" src="${emp.picture.large}" alt="profile picture">
+    document.getElementById('modal-close-btn').addEventListener('click', () => {
+        document.querySelector(".modal-container").style.display = "none";
+        console.log("Modal should be closed now");
+    })
+}
+
+function updateModal(emp) {
+    document.querySelector('.modal-info-container').innerHTML = '';
+    
+
+    document.querySelector('.modal-info-container') += `
+            <img class="modal-img" src="${emp.picture.large}" alt="profile picture">
                 <h3 id="name" class="modal-name cap">${emp.name.first} ${emp.name.last}</h3>
                 <p class="modal-text">${emp.email}</p>
                 <p class="modal-text cap">${emp.location.city}</p>
-            <hr>
+                <hr>
                 <p class="modal-text">${emp.cell}</p>
                 <p class="modal-text">${emp.location.street.number} ${emp.location.stree.name}, ${emp.location.city}, ${emp.location.state} ${emp.location.postcode}</p>
                 <p class="modal-text">Birthday: ${emp.dob.date}</p>
-            </div>
-        </div>
-        
-        
-        `
-    )
+            `;
+            document.querySelector('.modal-info-container').insertAdjacentHTML('afterbegin', modalInfoContainer);
 
-    
+}
 
-    body.appendChild(bg);
+function addClickabilityToCards(data) {
+    document.querySelectorAll('.card').forEach((card) => {
+        addEventListener('click', () => {
+            document.querySelector(".modal-container").style.display = "block";
+            updateModal(i);
+        })
 
-    let close = document.createElement("span");
-    close.className = "modal-close-btn";
-    close.insertAdjacentHTML('beforeend', 'x');
-    close.addEventListener('click', function () {
-        let overlay = body.querySelector(".modal-container");
-        let closebtn = parent.querySelector(".modal-close-btn");
-
-        body.removeChild(overlay);
-
-        el.removeChild(closebtn);
-    });
-
-
+    })
 }
 
 
 
-document.querySelector('.card').addEventListener('click', (e) => {
-    generateModal();
-})
 
 
 
-/*  */
+// below code is all the information that will change for every modal pop up inside of the modal-info-container div
+/* <img class="modal-img" src="${emp.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${emp.name.first} ${emp.name.last}</h3>
+            <p class="modal-text">${emp.email}</p>
+            <p class="modal-text cap">${emp.location.city}</p>
+        <hr>
+            <p class="modal-text">${emp.cell}</p>
+            <p class="modal-text">${emp.location.street.number} ${emp.location.stree.name}, ${emp.location.city}, ${emp.location.state} ${emp.location.postcode}</p>
+            <p class="modal-text">Birthday: ${emp.dob.date}</p> */
