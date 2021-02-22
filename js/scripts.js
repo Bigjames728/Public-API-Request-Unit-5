406202a2db85ac3736ad1e006e27307798b60e56
Bigjames728 <bigjames728@gmail.com>
2021-02-22 12:31:06 -0800

    Adding HTML to generateModal function

--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,7 @@ function generatePerson(data) {
 
 
 
-fetchData('https://randomuser.me/api/?results=12&inc=name,location,email,picture')
+fetchData('https://randomuser.me/api/?results=12&inc=name,gender,location,email,picture,cell,dob,nat&nat=au,br,ca,us')
 
 
 
@@ -42,10 +42,36 @@ fetchData('https://randomuser.me/api/?results=12&inc=name,location,email,picture
 
 
 function generateModal(id) {
+    let modalContainer = document.querySelector(".modal-container");
     let el = document.getElementById(id);
     let body = document.querySelector("body");
     let bg = document.createElement("div");
     bg.className = "modal-container";
+
+    let html = '';
+
+    data.results.forEach((emp) => 
+        html += `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${emp.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${emp.name.first} ${emp.name.last}</h3>
+                <p class="modal-text">${emp.email}</p>
+                <p class="modal-text cap">${emp.location.city}</p>
+            <hr>
+                <p class="modal-text">${emp.cell}</p>
+                <p class="modal-text">${emp.location.street.number} ${emp.location.stree.name}, ${emp.location.city}, ${emp.location.state} ${emp.location.postcode}</p>
+                <p class="modal-text">Birthday: ${emp.dob.date}</p>
+            </div>
+        </div>
+        
+        
+        `
+    )
+
+    
 
     body.appendChild(bg);
 
@@ -72,17 +98,4 @@ document.querySelector('.card').addEventListener('click', (e) => {
 
 
 
-/* <div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-            <h3 id="name" class="modal-name cap">name</h3>
-            <p class="modal-text">email</p>
-            <p class="modal-text cap">city</p>
-            <hr>
-            <p class="modal-text">(555) 555-5555</p>
-            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-            <p class="modal-text">Birthday: 10/21/2015</p>
-        </div>
-    </div> */
+/*  */
